8b85e80141f9bb4f0d5c11d06669ffc11b461d2d
Joan Miquel <joanhey@kumbiaphp.com>
2025-05-15 22:06:27 +0200

    Fix code

--- fibonacci/php/code.php
+++ fibonacci/php/code.php
@@ -4,8 +4,8 @@ function fibonacci(int $n): int
 {
     return match($n) {
         0,1 => $n,
-        default fibonacci($n - 1) + fibonacci($n - 2)
-    }
+        default => fibonacci($n - 1) + fibonacci($n - 2)
+    };
 }
 
 $u = (int) $argv[1];
